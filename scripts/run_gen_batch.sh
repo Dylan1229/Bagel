1da7555dbadab27f6c3607980de4e30cd7f73cdc
Dylan1229 <yvanphys@gmail.com>
2025-10-24 03:18:46 +0000

    update scripts

--- scripts/run_gen_batch.sh
+++ scripts/run_gen_batch.sh
@@ -3,17 +3,23 @@
 conda init
 conda activate /hdd/yuke/fanjiang/conda_env/bagel
 cd /scr/dataset/yuke/fanjiang/repo/unified-model/Bagel
-export PYTHONPATH=$PWD:$PYTHONPATH
 
+set -x 
+export PYTHONPATH=$PWD:$PYTHONPATH
 export cuda_visible_devices=0
 
-python ./scripts/inference_batch/image_gen_batch.py \
-  --model_path ./models/BAGEL-7B-MoT \
-  --prompt_pth ./data_profile/prompt/text2image.csv \
-  --output ./results/image_gen \
-  --seed 42 \
-  --think False \
-  --do_sample False \
-  --image_shapes 256 256
+timestamp=$(date +%Y%m%d_%H%M%S)
+LOGFILE="./scripts/logs/run_gen_batch_${timestamp}.log"
+
+exec >"$LOGFILE" 2>&1
+  python ./scripts/inference_batch/image_gen_batch.py \
+    --model_path ./models/BAGEL-7B-MoT \
+    --prompt_pth ./data_profile/prompt/text2image.csv \
+    --output ./results/image_gen \
+    --seed 42 \
+    --think False \
+    --do_sample False \
+    --image_shapes 256 256
+
 
 
